aaa63700fe477df64a3f2eea1ee756b35a5efbe1
Dpaslord <Davidpascuallorenzo@gmail.com>
2026-04-21 11:12:51 +0200

    Initial Commit

---
+++ Formulario con Base de Datos/Formulario_con_Base_de_DatosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Formulario_con_Base_de_DatosApp.swift
+//  Formulario con Base de Datos
+//
+//  Created by David Pascual Lorenzo on 21/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct Formulario_con_Base_de_DatosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
